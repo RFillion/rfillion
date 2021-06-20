97f7f82c2487d0d5b0836311e42f48b2a41e1eff
Raphaël Fillion <34820473+RaphY18@users.noreply.github.com>
2021-06-20 17:36:06 -0400

    scrolltrigger
    added a scrolltriger when scrolling further in the page

--- script/main.js
+++ script/main.js
@@ -1,3 +1,4 @@
+//hamburger menu
 let menuBtn = document.querySelector('.menu-btn');
 let menuItems = document.querySelector('.menu-items');
 
@@ -5,3 +6,16 @@ menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('open');
     menuItems.classList.toggle('open');
 });
+
+//ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to('.header', {
+    scrollTrigger: {
+        start: 'top',
+        trigger: '.header',
+        toggleActions: 'play none none reverse',
+    },
+    backgroundColor: 'white',
+    duration: 0.2,
+})
