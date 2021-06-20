bdefda18977e9cfda1f8a93aecd9127d8c7b34c1
Raphaël Fillion <34820473+RaphY18@users.noreply.github.com>
2021-06-20 17:42:30 -0400

    some fix for the hamburger menu

--- script/main.js
+++ script/main.js
@@ -1,11 +1,21 @@
 //hamburger menu
 let menuBtn = document.querySelector('.menu-btn');
 let menuItems = document.querySelector('.menu-items');
+let menuItem = document.querySelectorAll('.menu-item');
 
 menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('open');
     menuItems.classList.toggle('open');
 });
+
+menuItem.forEach(item => {
+    item.addEventListener('click', () => {
+        if(menuBtn.classList.contains('open')) {
+            menuBtn.classList.toggle('open');
+            menuItems.classList.toggle('open');
+        }
+    })
+})
 
 //ScrollTrigger
 gsap.registerPlugin(ScrollTrigger);
