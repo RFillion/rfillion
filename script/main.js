0f29b9e6462d7a343fcfa76dfbaee658223f1560
Raphaël Fillion <rfillion@blanko.ca>
2022-06-23 16:26:06 -0400

    done main content

--- script/main.js
+++ script/main.js
@@ -7,15 +7,22 @@ let toggleBottom = document.querySelector('.mobile-toggle .bottom')
 
 let mobileNav = document.querySelector('.mobileNav')
 
+let homeContent = document.querySelector('.container-content')
+
 let mobileNavItems = document.querySelectorAll('.mobileNav--navigation-item')
 
 mobileToggle.addEventListener('click', () => {
     if(!(mobileToggle.classList.contains('active'))) {
         mobileToggle.classList.add('active')
         mobileNav.style.visibility = 'visible'
-        gsap.fromTo(mobileNav,
+
+        gsap.timeline()
+        .fromTo(mobileNav,
             {y: '100%', opacity: 0},
             {y: 0, opacity: 1, duration: 1, ease: 'expo.out'})
+        .fromTo(homeContent,
+            {opacity: 1},
+            {opacity: 0, duration: .5, ease: 'expo.out'}, .05)
     
         gsap.timeline()
         .fromTo(toggleTop,
@@ -35,11 +42,15 @@ mobileToggle.addEventListener('click', () => {
     } else {
         mobileToggle.classList.remove('active')
 
-        gsap.fromTo(mobileNav,
+        gsap.timeline()
+        .fromTo(mobileNav,
             {y: 0, opacity: 1},
             {y: '100%', opacity: 0, duration: 1, ease: 'expo.out', onComplete: () => {
                 mobileNav.style.visibility = 'hidden'
         }})
+        .fromTo(homeContent, 
+            {opacity: 0},
+            {opacity: 1, duration: .5, ease: 'expo.out'}, .1)
 
         gsap.timeline()
         .fromTo(toggleTop,
@@ -47,14 +58,14 @@ mobileToggle.addEventListener('click', () => {
             {y: -5, duration: .8, ease: 'expo.out'})
         .fromTo(toggleTop,
             {width: '30px'},
-            {width: '20px', duration: .3, ease: 'expo.out'})
+            {width: '20px', duration: .3, ease: 'expo.out'}, .3)
         gsap.timeline()
         .fromTo(toggleBottom,
             {y: -2},
             {y: 5, duration: .8, ease: 'expo.out'})
         .fromTo(toggleBottom,
             {width: '30px'},
-            {width: '20px', duration: .3, ease: 'expo.out'})
+            {width: '20px', duration: .3, ease: 'expo.out'}, .3)
     }
 })
 
@@ -62,11 +73,15 @@ mobileNavItems.forEach(item => {
     item.addEventListener('click', () => {
         mobileToggle.classList.remove('active')
 
-        gsap.fromTo(mobileNav, 
+        gsap.timeline()
+        .fromTo(mobileNav, 
             {opacity: 1},
             {opacity: 0, duration: .3, ease: 'expo.out', onComplete: () => {
                 mobileNav.style.visibility = 'hidden'
-            }})
+        }})
+        .fromTo(homeContent,
+            {opacity: 0},
+            {opacity: 1, duration: .5, ease: 'expo.out'}, .05)
         
         gsap.timeline()
         .fromTo(toggleTop,
