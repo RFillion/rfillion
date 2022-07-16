d745d19df4950f12d3776d01e19fb6993113304c
Raphaël Fillion <rfillion@blanko.ca>
2022-07-16 00:42:45 -0400

    reformat

--- script/main.js
+++ script/main.js
@@ -12,60 +12,62 @@ let homeContent = document.querySelector('.container-content')
 let mobileNavItems = document.querySelectorAll('.mobileNav--navigation-item')
 
 mobileToggle.addEventListener('click', () => {
-    if(!(mobileToggle.classList.contains('active'))) {
+    if (!(mobileToggle.classList.contains('active'))) {
         mobileToggle.classList.add('active')
         mobileNav.style.visibility = 'visible'
 
         gsap.timeline()
-        .fromTo(mobileNav,
-            {y: '100%', opacity: 0},
-            {y: 0, opacity: 1, duration: 1, ease: 'expo.out'})
-        .fromTo(homeContent,
-            {opacity: 1},
-            {opacity: 0, duration: .5, ease: 'expo.out'}, .05)
-    
+            .fromTo(mobileNav,
+                { y: '100%', opacity: 0 },
+                { y: 0, opacity: 1, duration: 1, ease: 'expo.out' })
+            .fromTo(homeContent,
+                { opacity: 1 },
+                { opacity: 0, duration: .5, ease: 'expo.out' }, .05)
+
         gsap.timeline()
-        .fromTo(toggleTop,
-            {width: '20px'},
-            {width: '30px', duration: .3, ease: 'expo.out'})
-        .fromTo(toggleTop,
-            {y: -5},
-            {y: 2, duration: .8, ease: 'expo.out'})
-        
+            .fromTo(toggleTop,
+                { width: '20px' },
+                { width: '30px', duration: .3, ease: 'expo.out' })
+            .fromTo(toggleTop,
+                { y: -5 },
+                { y: 2, duration: .8, ease: 'expo.out' })
+
         gsap.timeline()
-        .fromTo(toggleBottom,
-            {width: '20px'},
-            {width: '30px', duration: .3, ease: 'expo.out'})
-        .fromTo(toggleBottom,
-            {y: 5},
-            {y: -2, duration: .8, ease: 'expo.out'})
+            .fromTo(toggleBottom,
+                { width: '20px' },
+                { width: '30px', duration: .3, ease: 'expo.out' })
+            .fromTo(toggleBottom,
+                { y: 5 },
+                { y: -2, duration: .8, ease: 'expo.out' })
     } else {
         mobileToggle.classList.remove('active')
 
         gsap.timeline()
-        .fromTo(mobileNav,
-            {y: 0, opacity: 1},
-            {y: '100%', opacity: 0, duration: 1, ease: 'expo.out', onComplete: () => {
-                mobileNav.style.visibility = 'hidden'
-        }})
-        .fromTo(homeContent, 
-            {opacity: 0},
-            {opacity: 1, duration: .5, ease: 'expo.out'}, .1)
+            .fromTo(mobileNav,
+                { y: 0, opacity: 1 },
+                {
+                    y: '100%', opacity: 0, duration: 1, ease: 'expo.out', onComplete: () => {
+                        mobileNav.style.visibility = 'hidden'
+                    }
+                })
+            .fromTo(homeContent,
+                { opacity: 0 },
+                { opacity: 1, duration: .5, ease: 'expo.out' }, .1)
 
         gsap.timeline()
-        .fromTo(toggleTop,
-            {y: 2},
-            {y: -5, duration: .8, ease: 'expo.out'})
-        .fromTo(toggleTop,
-            {width: '30px'},
-            {width: '20px', duration: .3, ease: 'expo.out'}, .3)
+            .fromTo(toggleTop,
+                { y: 2 },
+                { y: -5, duration: .8, ease: 'expo.out' })
+            .fromTo(toggleTop,
+                { width: '30px' },
+                { width: '20px', duration: .3, ease: 'expo.out' }, .3)
         gsap.timeline()
-        .fromTo(toggleBottom,
-            {y: -2},
-            {y: 5, duration: .8, ease: 'expo.out'})
-        .fromTo(toggleBottom,
-            {width: '30px'},
-            {width: '20px', duration: .3, ease: 'expo.out'}, .3)
+            .fromTo(toggleBottom,
+                { y: -2 },
+                { y: 5, duration: .8, ease: 'expo.out' })
+            .fromTo(toggleBottom,
+                { width: '30px' },
+                { width: '20px', duration: .3, ease: 'expo.out' }, .3)
     }
 })
 
@@ -74,30 +76,32 @@ mobileNavItems.forEach(item => {
         mobileToggle.classList.remove('active')
 
         gsap.timeline()
-        .fromTo(mobileNav, 
-            {opacity: 1},
-            {opacity: 0, duration: .3, ease: 'expo.out', onComplete: () => {
-                mobileNav.style.visibility = 'hidden'
-        }})
-        .fromTo(homeContent,
-            {opacity: 0},
-            {opacity: 1, duration: .5, ease: 'expo.out'}, .05)
-        
+            .fromTo(mobileNav,
+                { opacity: 1 },
+                {
+                    opacity: 0, duration: .3, ease: 'expo.out', onComplete: () => {
+                        mobileNav.style.visibility = 'hidden'
+                    }
+                })
+            .fromTo(homeContent,
+                { opacity: 0 },
+                { opacity: 1, duration: .5, ease: 'expo.out' }, .05)
+
         gsap.timeline()
-        .fromTo(toggleTop,
-            {y: 2},
-            {y: -5, duration: .8, ease: 'expo.out'})
-        .fromTo(toggleTop,
-            {width: '30px'},
-            {width: '20px', duration: .3, ease: 'expo.out'})
+            .fromTo(toggleTop,
+                { y: 2 },
+                { y: -5, duration: .8, ease: 'expo.out' })
+            .fromTo(toggleTop,
+                { width: '30px' },
+                { width: '20px', duration: .3, ease: 'expo.out' })
         gsap.timeline()
-        .fromTo(toggleBottom,
-            {y: -2},
-            {y: 5, duration: .8, ease: 'expo.out'})
-        .fromTo(toggleBottom,
-            {width: '30px'},
-            {width: '20px', duration: .3, ease: 'expo.out'})
-        
+            .fromTo(toggleBottom,
+                { y: -2 },
+                { y: 5, duration: .8, ease: 'expo.out' })
+            .fromTo(toggleBottom,
+                { width: '30px' },
+                { width: '20px', duration: .3, ease: 'expo.out' })
+
     })
 })
 
